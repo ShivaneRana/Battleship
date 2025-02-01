ac132abd80480ee884b61c4ae7aff9d1bc055afe
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-01 16:39:45 +0530

    isSunk function added

--- src/module/ship.js
+++ src/module/ship.js
@@ -1,12 +1,17 @@
 class Ship{
     constructor(){
-        this.length = 5;
+        this.length = 3;
         this.hitTaken = 0;
         this.sank = false;
     }
 
     hit(){
         this.hitTaken++;
+    }
+
+    isSunk(){
+        this.sank = this.hitTaken === this.length ? true : false;
+        return this.sank;
     }
 }
 
