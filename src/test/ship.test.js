880eeed5f6a936b6b34a5974a62ca8742d0400f1
Shivane Rana <72935198+ShivaneRana@users.noreply.github.com>
2025-02-01 16:27:50 +0530

    test for tracking length,hitTaken and sank variable

--- src/test/ship.test.js
+++ src/test/ship.test.js
@@ -2,7 +2,15 @@ const {Ship} = require('../module/ship.js');
 
 describe("Ship class",() => {
 
-    test("ship class is defined",() => {
+    test("Ship class is defined",() => {
         expect(Ship).toBeDefined();
     })
+
+    test("Ship class has lenght,hitTaken and Sank",() => {
+        expect(new Ship().length).toBeDefined();
+        expect(new Ship().hitTaken).toBeDefined();
+        expect(new Ship().sank).toBeDefined();
+    })
+
+    
 })
